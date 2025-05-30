a0b304fc2ff7de718344b1d4ace1b3471e464896
Kevin Blair <kblair40@gmail.com>
2025-05-30 11:11:39 -0500

    Add drop and clear handlers

--- src/store/ImagesContext.tsx
+++ src/store/ImagesContext.tsx
@@ -1,26 +1,46 @@
 "use client";
 
 import React, { useState, createContext } from "react";
+import type { FileRejection } from "react-dropzone";
 
 interface ImagesContextType {
   acceptedImages: File[];
-  rejectedImages: File[];
+  rejectedImages: FileRejection[];
+  handleDroppedImages: (acc: File[], rej: FileRejection[]) => void;
+  clearImages: () => void;
 }
 
 export const ImagesContext = createContext<ImagesContextType>({
   acceptedImages: [],
   rejectedImages: [],
+  handleDroppedImages: (acc: File[], rej: FileRejection[]) => {},
+  clearImages: () => {},
 });
 
 const ImagesContextProvider = ({ children }: React.PropsWithChildren) => {
   const [acceptedImages, setAcceptedImages] = useState<File[]>([]);
-  const [rejectedImages, setRejectedImages] = useState<File[]>([]);
+  const [rejectedImages, setRejectedImages] = useState<FileRejection[]>([]);
+
+  const handleDroppedImages = (
+    acceptedImages: File[],
+    rejectedImages: FileRejection[]
+  ) => {
+    setAcceptedImages(acceptedImages);
+    setRejectedImages(rejectedImages);
+  };
+
+  const clearImages = () => {
+    setAcceptedImages([]);
+    setRejectedImages([]);
+  };
 
   return (
     <ImagesContext.Provider
       value={{
         acceptedImages,
         rejectedImages,
+        handleDroppedImages,
+        clearImages,
       }}
     >
       {children}
