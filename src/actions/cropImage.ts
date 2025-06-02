5d82f3b62e8b4e66d4ccf8cac2b6c886392a2bc1
Kevin Blair <kblair40@gmail.com>
2025-06-02 15:28:07 -0500

    Add crop image server action

---
+++ src/actions/cropImage.ts
@@ -0,0 +1,66 @@
+"use server";
+
+import sharp from "sharp";
+import type {
+  OutputInfo,
+  // OutputOptions
+  ResizeOptions,
+} from "sharp";
+import { imageSize } from "image-size";
+import type { PixelCrop } from "react-image-crop";
+import type { ISizeCalculationResult } from "image-size/types/interface";
+
+import type { Dimensions } from "@/lib/image-types";
+import { resizeImage as _resizeImage } from "@/lib/image-utils";
+
+export type OptimizedMetadata = {
+  metadata: ISizeCalculationResult & OutputInfo;
+  dataUrl: string;
+};
+
+export async function cropImage(
+  dataUrl: string,
+  crop: PixelCrop
+): Promise<OptimizedMetadata | null> {
+  try {
+    const buf = Buffer.from(dataUrl.split(",")[1], "base64");
+    const metadata = await imageSize(buf);
+    console.log("metadata:", metadata);
+
+    if (!metadata.type) {
+      return null;
+    }
+
+    const img = sharp(buf).extract({
+      left: crop.x,
+      top: crop.y,
+      width: crop.width,
+      height: crop.height,
+    });
+
+    const bufOut = await img.toBuffer();
+
+    const b64Url = bufOut.toString("base64");
+    const mdOut = await imageSize(bufOut);
+
+    const file: OutputInfo = await new Promise(async (resolve, reject) => {
+      try {
+        img.toFile("tmp", (err, info) => {
+          console.log("\nErr/Info:", { err, info }, "\n");
+          resolve(info);
+        });
+      } catch (e) {
+        console.log("\nError creating file:", e, "n");
+        reject(e);
+      }
+    });
+    console.log("\nFile:", file);
+
+    console.log("\n");
+    return { dataUrl: b64Url, metadata: Object.assign(mdOut, file) };
+    // return { dataUrl: b64Url, metadata: mdOut };
+  } catch (e) {
+    console.log("\nError extracting image metadata:", e, "\n");
+    return null;
+  }
+}
