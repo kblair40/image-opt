d19a08867d794d6038971889dfac74066275512d
Kevin Blair <kblair40@gmail.com>
2025-06-04 05:24:43 -0500

    Add serializeMetadata helper fn

--- src/lib/server-image-utils.ts
+++ src/lib/server-image-utils.ts
@@ -1,12 +1,27 @@
 import sharp from "sharp";
 import type { ResizeOptions } from "sharp";
 
-import type { Metadata, SerializedMetadata } from "./image-types";
+import type { Metadata, SerializedMetadata, BufferField } from "./image-types";
 
 export function resizeImage(img: sharp.Sharp, options: ResizeOptions) {
   return img.resize(options);
 }
 
-export function serializeMetadata(md: Metadata): SerializedMetadata | null {
-  return null;
+const BUFFER_FIELDS: BufferField[] = [
+  "exif",
+  "icc",
+  "iptc",
+  "xmp",
+  "tifftagPhotoshop",
+];
+
+export function serializeMetadata(md: Metadata): SerializedMetadata {
+  const serFields: any = {};
+  for (const key of BUFFER_FIELDS) {
+    if (md[key] !== undefined) {
+      serFields[key] = md[key].toString();
+    }
+  }
+
+  return { ...md, ...serFields } as SerializedMetadata;
 }
