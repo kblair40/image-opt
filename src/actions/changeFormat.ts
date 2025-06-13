77ef7276b6c712c3069f237ee5a8df7b453faa98
Kevin Blair <kblair40@gmail.com>
2025-06-13 15:15:14 -0500

    Add changeFormat serverFn

---
+++ src/actions/changeFormat.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import sharp from "sharp";
+
+import type { AllowedImageFormat, SerializedMetadata } from "@/lib/image-types";
+import { serializeMetadata } from "@/lib/server-image-utils";
+
+export async function changeFormat(
+  dataUrl: string,
+  type: AllowedImageFormat
+): Promise<{ metadata: SerializedMetadata; dataUrl: string } | null> {
+  try {
+    console.log("\nchangeFormat args:", { dataUrl });
+    const buf = Buffer.from(dataUrl.split(",")[1], "base64");
+    const img = sharp(buf).toFormat(type, { quality: 100 });
+
+    const bufOut = await img.toBuffer();
+    const b64Url = bufOut.toString("base64");
+    const mdOut = await img.metadata();
+    console.log("\nb64Url:", b64Url);
+
+    console.log("\n");
+    // return serializeMetadata(mdOut)
+    return {
+      dataUrl: b64Url,
+      metadata: serializeMetadata(mdOut),
+    };
+  } catch (e) {
+    console.log("\nError extracting image metadata:", e, "\n");
+    return null;
+  }
+}
